41af28a6349753e58b5a7e4bf78ca88f0d9038d7
Avivar101 <63792174+Avivar101@users.noreply.github.com>
2020-09-18 12:39:10 +0100

    icons

--- pureweb/src/components/Header.vue
+++ pureweb/src/components/Header.vue
@@ -1,7 +1,23 @@
 <template>
-<div class="header">
-    <h1>This is the header</h1>
-</div>
+<header>
+    <div class="hd-top">
+        <ul>
+            <li>
+                <img class="social-icon1" src="../assets/facebook-f-brands.svg" alt="fb" />
+            </li>
+            <li>
+                <img class="social-icon2" src="../assets/instagram-brands.svg" alt="inst" />
+            </li>
+            <li>
+                <img class="social-icon3" src="../assets/twitter-brands.svg" alt="twtt" />
+            </li>
+        </ul>
+    </div>
+    <div class="header">
+        <h1>This is the header</h1>
+    </div>
+    <div class="hd-bottom"></div>
+</header>
 </template>
 
 <script>
@@ -9,6 +25,3 @@ export default {
     name: "Header",
 };
 </script>
-
-<style scoped>
-</style>
