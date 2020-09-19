e5bb4b5432f5aa4428b707e56fabd742cf5bcd5e
Avivar101 <63792174+Avivar101@users.noreply.github.com>
2020-09-19 22:07:58 +0100

    img

--- pureweb/src/components/HeadSlider.vue
+++ pureweb/src/components/HeadSlider.vue
@@ -1,9 +1,23 @@
 <template>
 <section>
-    <div class="slider">
-        <div class="img-slidr"></div>
-        <div class="pad slidr-botm"></div>
+    <div class="slide">
+        <div class="slider1">
+            <div class="img-slidr">
+                <div class="container">
+                    <h4>STAY AHEAD OF THE DIGITAL GAME</h4>
+                    <h2>
+                        WE ARE
+                        <span>PUREWEB</span>
+                    </h2>
+                    <P>
+                        Delivering digital transformation that will enable you to power efficiently,
+                        <br />divine growth and build credibility for your business
+                    </P>
+                </div>
+            </div>
+        </div>
     </div>
+    <div class="pad slidr-botm"></div>
 </section>
 </template>
 
