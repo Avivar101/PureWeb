652035f11dce1aba6ec44a8e3483efc73f96a8c9
Avivar101 <63792174+Avivar101@users.noreply.github.com>
2020-09-21 15:26:34 +0100

    style

--- pureweb/src/components/HeadBody.vue
+++ pureweb/src/components/HeadBody.vue
@@ -72,13 +72,47 @@
             <div class="bodTxt">
                 <h1>A Glance On Our Services</h1>
                 <p>
-                    We are a dynamic and business focused ICT firm. We boast of a unique set of skills and competencies as required for the teeming needs of our clients.
-                    One of our "claims to fame" is our ability to get you online quickly. Building a website is a complex, time-consuming undertaking.
+                    We are a dynamic and business focused ICT firm. We boast of a
+                    <strong>unique set of skills</strong> and competencies as required for the teeming needs of our clients.
+                    One of our
+                    <strong>"claims to fame"</strong> is our ability to get you online quickly. Building a website is a complex, time-consuming undertaking.
                 </p>
             </div>
-            <div class="bodBtn"></div>
+            <div class="bodBtn">
+                <div class="btnClor ht">Web Design</div>
+                <div class="btnClor bt">Software</div>
+                <div class="btnClor bt">Branding</div>
+                <div class="btnClor bt">Application</div>
+                <div class="btnClor ht">Social Media</div>
+            </div>
         </div>
-        <div class="pad bod2"></div>
+        <div class="pad bod2">
+            <div class="nte">
+                <img src="../assets/images/tab1.png" />
+                <h3>We Do Custom Web Design</h3>
+
+                <p>A successful design demands a conceptual clarity and an ability to visualize the overall solution within the periphery of specific industry domains and their business processes.</p>
+                <p>
+                    The technical team at PureWeb is a seasoned group of professionals who can visualize the most complex client requirements and can translate them into a simplistic and aesthetic solution – A corporate website that will help announce your arrival on the scene.
+                    We deliver a product that makes your organization the leader in its segment while carving a niche for itself in the web world.
+                </p>
+                <router-link to tag="li">Contact Us</router-link>
+            </div>
+        </div>
+    </div>
+    <div class="last">
+        <div class="pad plast">
+            <div class="plastTxt">
+                <h3>We Provide Services For</h3>
+                <p>We Provide Web Solutions, As Well As Software Solutions For So Many Industries, Below Are Few Industries We Service</p>
+            </div>
+            <div class="plastBox">
+                <div class="Box box1"></div>
+                <div class="Box box2"></div>
+                <div class="Box box3"></div>
+            </div>
+        </div>
+        <div class="pad plast2"></div>
     </div>
 </section>
 </template>
